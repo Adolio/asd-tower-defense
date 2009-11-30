f34f42c1093032af5bbdc277bbe9237d4f69ff99
lazhar.farjallah <lazhar.farjallah@02f27a40-d44b-11de-ba7e-09ca73f1295e>
2009-11-30 21:28:52 +0000

    Implémentation du maillage. Création du graphe avec les noeuds et les arcs pondérés. Création d'un document excel pour tester les performances en temps et en mémoire.

---
+++ trunk/src/TestMaillage.java
@@ -0,0 +1,72 @@
+
+import java.util.Scanner;
+
+import models.maillage.*;
+
+/**
+ * Fichier  : Test.java
+ * <p>
+ * Encodage : UTF-8
+ *
+ * <p> Cette classe...
+ * <p> Remarques :
+ *
+ * @author  Lazhar Farjallah
+ * @version 30 nov. 2009
+ * @since   jdk1.6.0_16
+ */
+public class TestMaillage
+{
+    
+    private static Scanner scanner = new Scanner(System.in);
+    
+    /**
+     * Programme principal.
+     * 
+     * @param args Les arguments fournis au programme principal.
+     */
+    public static void main(String[] args)
+    {
+        //----------------------------------------------------------------------------
+        // Partie déclarative (constantes, variables).
+        //----------------------------------------------------------------------------
+        Maillage maillage;
+        long temps;
+        
+        //----------------------------------------------------------------------------
+        // Présentation et entrée des données par l'utilisateur.
+        //----------------------------------------------------------------------------
+        System.out.print("\n<----------------------------------------------------\n"
+                + " Test du maillage : ......... \n"
+                + "-----------------------------------------------------\n\n");
+        
+        //----------------------------------------------------------------------------
+        // Calcul et affichage du résultat.
+        //----------------------------------------------------------------------------
+        temps = System.currentTimeMillis();
+        maillage = new Maillage(500, 400);
+        temps = System.currentTimeMillis() - temps;
+        System.out.println(maillage + "\n");
+        System.out.println("Temps de génération du maillage : " +
+                           temps / 1000.0 + " [sec.]");
+        System.out.println("Mémoire utilisée                : " +
+                           Math.round(memoireUtilisee() / (1024.0 * 1024.0) * 100)
+                                                                 / 100.0 + " [Mb]");
+        
+        //----------------------------------------------------------------------------
+        // Fin du programme.
+        //----------------------------------------------------------------------------
+        System.out.print("\n\nAppuyez sur une touche pour terminer...");
+        scanner.nextLine();
+        System.out.print("\n-----------------------------------------------------\n"
+                + "Fin du programme principal.\n"
+                + "----------------------------------------------------->\n\n");
+    }
+    
+    private static long memoireUtilisee()
+    {
+        Runtime rt = Runtime.getRuntime();
+        return rt.totalMemory() - rt.freeMemory();
+    }
+    
+}
--- trunk/src/models/maillage/Maillage.java
+++ trunk/src/models/maillage/Maillage.java
@@ -1,6 +1,9 @@
 package models.maillage;
-import java.util.Vector;
+
+import java.util.*;
 import java.awt.*;
+
+import org.jgrapht.WeightedGraph;
 import org.jgrapht.graph.*;
 
 /**
@@ -20,115 +23,165 @@ import org.jgrapht.graph.*;
  */
 public class Maillage
 {
-   
-   private int largeurPixels;
-   private int hauteurPixels;
-   private DefaultDirectedWeightedGraph<Noeud, DefaultEdge> graphe;
-   
-   /**
-    * LA CONSTRUCTION SE FAIT SIMPLEMENT AVEC UNE LARGEUR ET UNE HAUTEUR
-    * 
-    * DANS CE RECTANGLE TU MAILLE COMME TU VEUX...
-    * 
-    * @param largeurPixels
-    * @param hauteurPixels
-    * @param tours
-    * @param murs
-    * @throws IllegalArgumentException
-    */
-   public Maillage (int largeurPixels, int hauteurPixels, 
-		   			Vector<Rectangle> zonesDesactivees) 
-   					throws IllegalArgumentException
-   {
-      this.largeurPixels = largeurPixels;
-      this.hauteurPixels = hauteurPixels;
-      graphe = construireGraphe(zonesDesactivees);
-   }
-   
-   /**
-    * Getter pour le champ <tt>largeurPixels</tt>
-    * 
-    * @return La valeur du champ <tt>largeurPixels</tt>
-    */
-   public int getLargeurPixels ()
-   {
-      return largeurPixels;
-   }
-   
-   /**
-    * Getter pour le champ <tt>hauteurPixels</tt>
-    * 
-    * @return La valeur du champ <tt>hauteurPixels</tt>
-    */
-   public int getHauteurPixels ()
-   {
-      return hauteurPixels;
-   }
-   
-   /**
-    * Setter pour le champ <tt>largeurPixels</tt>
-    * 
-    * @param largeurPixels
-    *        La valeur qu'on veut attribuer au champ <tt>largeurPixels</tt>
-    */
-   public void setLargeurPixels (int largeurPixels)
-   {
-      this.largeurPixels = largeurPixels;
-   }
-   
-   /**
-    * Setter pour le champ <tt>hauteurPixels</tt>
-    * 
-    * @param hauteurPixels
-    *        La valeur qu'on veut attribuer au champ <tt>hauteurPixels</tt>
-    */
-   public void setHauteurPixels (int hauteurPixels)
-   {
-      this.hauteurPixels = hauteurPixels;
-   }
-   
-   /**
-    * DESACTIVE UNE ZONE --> IDEPENDANT DES TOURS !!!
-    * 
-    * @param rectangle la zone a desactiver
-    * @throws IllegalArgumentException
-    */
-   public void activerNoeuds (Rectangle rectangle) throws IllegalArgumentException
-   {
-      // TODO : activer les noeuds dans la zone donnee.
-   }
-   
-   /**
-    * DESACTIVE UNE ZONE --> IDEPENDANT DES TOURS !!!
-    * 
-    * @param rectangle la zone a desactiver
-    * @throws IllegalArgumentException
-    */
-   public void desactiverNoeuds (Rectangle rectangle) throws IllegalArgumentException
-   {
-      // TODO : desactiver les noeuds dans la zone donnee.
-   }
-   
-   /**
-    * @param tours
-    * @param murs
-    * @return
-    */
-   private static DefaultDirectedWeightedGraph<Noeud, DefaultEdge> 
-   	construireGraphe (Vector<Rectangle> zoneDesactivee)
-   	throws IllegalArgumentException
-   {
-      DefaultDirectedWeightedGraph<Noeud, DefaultEdge> graphe =
-          new DefaultDirectedWeightedGraph<Noeud, DefaultEdge>(DefaultEdge.class);
-      
-      /*
-       * TODO : construire le graphe de base en fonction des tours et des murs.
-       * graphe.setEdgeWeight( graphe.addEdge( new Noeud(1, 2, false, false), new
-       * Noeud(3, 7, false, false) ), 3.5 );
-       */
+    
+    private final int DESACTIVE = Integer.MAX_VALUE;
+    private final int LARGEUR_NOEUD = 1;
+    private int largeurPixels;
+    private int hauteurPixels;
+    private ArrayList<Noeud> noeuds;
+    private DefaultDirectedWeightedGraph<Noeud, DefaultEdge> graphe;
+    
+    /**
+     * @param largeurPixels
+     * @param hauteurPixels
+     * @param tours
+     * @param murs
+     * @throws IllegalArgumentException
+     */
+    public Maillage(int largeurPixels,
+                    int hauteurPixels)
+    throws IllegalArgumentException
+    {
+        this.largeurPixels = largeurPixels;
+        this.hauteurPixels = hauteurPixels;
+        graphe = construireGraphe();
+    }
+    
+    /**
+     * Getter pour le champ <tt>largeurPixels</tt>
+     * 
+     * @return La valeur du champ <tt>largeurPixels</tt>
+     */
+    public int getLargeurPixels()
+    {
+        return largeurPixels;
+    }
+    
+    /**
+     * Getter pour le champ <tt>hauteurPixels</tt>
+     * 
+     * @return La valeur du champ <tt>hauteurPixels</tt>
+     */
+    public int getHauteurPixels()
+    {
+        return hauteurPixels;
+    }
+    
+    /**
+     * Setter pour le champ <tt>largeurPixels</tt>
+     * 
+     * @param largeurPixels
+     *            La valeur qu'on veut attribuer au champ <tt>largeurPixels</tt>
+     */
+    public void setLargeurPixels(int largeurPixels)
+    {
+        this.largeurPixels = largeurPixels;
+    }
+    
+    /**
+     * Setter pour le champ <tt>hauteurPixels</tt>
+     * 
+     * @param hauteurPixels
+     *            La valeur qu'on veut attribuer au champ <tt>hauteurPixels</tt>
+     */
+    public void setHauteurPixels(int hauteurPixels)
+    {
+        this.hauteurPixels = hauteurPixels;
+    }
+    
+    /**
+     * DESACTIVE UNE ZONE --> IDEPENDANT DES TOURS !!!
+     * 
+     * @param rectangle
+     *            la zone a activer
+     * @throws IllegalArgumentException
+     */
+    public void activerNoeuds(Rectangle rectangle) throws IllegalArgumentException
+    {
+        // TODO : activer les noeuds dans la zone donnee.
+        // Cela revient à mettre un poids sensé à chaque noeud concerné.
+    }
+    
+    /**
+     * DESACTIVE UNE ZONE --> IDEPENDANT DES TOURS !!!
+     * 
+     * @param rectangle
+     *            la zone a désactiver
+     * @throws IllegalArgumentException
+     */
+    public void desactiverNoeuds(Rectangle rectangle) throws IllegalArgumentException
+    {
+        // TODO : désactiver les noeuds dans la zone donnée.
+        // Pour cela, une idée serait de mettre simplement le poids des noeuds
+        // concernées à une valeur pseudo infinie.
+    }
+    
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "Largeur du maillage : " + largeurPixels + " pixels\n" +
+               "Hauteur du maillage : " + hauteurPixels + " pixels\n" +
+               "Représentation      : 1 noeud = " + LARGEUR_NOEUD + "x" +
+                                      LARGEUR_NOEUD + " pixels\n" +
+               //graphe.toString() + "\n" +
+               "nombre de noeuds    : " + graphe.vertexSet().size() + "\n" +
+               "nombre d'arcs       : " + graphe.edgeSet().size();
+    }
+    
+    /**
+     * @param tours
+     * @param murs
+     * @return
+     */
+    private DefaultDirectedWeightedGraph<Noeud,DefaultEdge> construireGraphe()
+    {
+        DefaultDirectedWeightedGraph<Noeud, DefaultEdge> graphe =
+            new DefaultDirectedWeightedGraph<Noeud, DefaultEdge>(DefaultWeightedEdge.class);
+        noeuds = new ArrayList<Noeud>();
+        
+        /*
+         * Ajouter les noeuds au graphe.
+         */
+        for (int x=0; x<largeurPixels; x+=LARGEUR_NOEUD) {
+            for (int y=0; y<hauteurPixels; y+=LARGEUR_NOEUD) {
+                noeuds.add(new Noeud(x, y));
+                graphe.addVertex(noeuds.get(noeuds.size() - 1));
+            }
+        }
+        
+        /*
+         * Ajouter les arcs horizontaux.
+         */
+        for (int x=0; x<largeurPixels / LARGEUR_NOEUD - 1; x+=1) {
+            for (int y=0; y<hauteurPixels / LARGEUR_NOEUD; y+=1) {
+                graphe.setEdgeWeight(
+                    graphe.addEdge(
+                        noeuds.get(x + y * largeurPixels / LARGEUR_NOEUD),
+                        noeuds.get(x + y * (largeurPixels / LARGEUR_NOEUD) + 1)
+                    ),
+                    WeightedGraph.DEFAULT_EDGE_WEIGHT
+                );
+            }
+        }
+        
+        /*
+         * Ajouter les arcs verticaux.
+         */
+        for (int x=0; x<largeurPixels / LARGEUR_NOEUD; x+=1) {
+            for (int y=0; y<hauteurPixels / LARGEUR_NOEUD - 1; y+=1) {
+                graphe.setEdgeWeight(
+                    graphe.addEdge(
+                        noeuds.get(x + y * largeurPixels / LARGEUR_NOEUD),
+                        noeuds.get(x + (y + 1) * (largeurPixels / LARGEUR_NOEUD))
+                    ),
+                    WeightedGraph.DEFAULT_EDGE_WEIGHT
+                );
+            }
+        }
 
-      return graphe;
-      
-   }
-   
+        return graphe;
+    }
+    
 }
