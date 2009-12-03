a7eb3a6ca9fff5a237ca366334dfc12c7c467508
lazhar.farjallah <lazhar.farjallah@02f27a40-d44b-11de-ba7e-09ca73f1295e>
2009-12-03 10:05:15 +0000

    Il fallait utiliser un graphe non orienté (PseudoGraph apparemment).

--- trunk/src/TestMaillage.java
+++ trunk/src/TestMaillage.java
@@ -46,7 +46,7 @@ public class TestMaillage
         // Calcul et affichage du résultat.
         //----------------------------------------------------------------------------
         temps = System.currentTimeMillis();
-        maillage = new Maillage(800, 600);
+        maillage = new Maillage(10, 10);
         temps = System.currentTimeMillis() - temps;
         System.out.println(maillage + "\n");
         System.out.println("Temps de génération du maillage : " +
@@ -56,7 +56,7 @@ public class TestMaillage
                                                                  / 100.0 + " [Mb]");
         
         temps = System.currentTimeMillis();
-        ArrayList<Point> points = maillage.plusCourtChemin(0, 0, 0, 0);
+        ArrayList<Point> points = maillage.plusCourtChemin(3, 3, 1, 2);
         temps = System.currentTimeMillis() - temps;
         System.out.println("Temps algorithme ACPC : " + temps / 1000.0 + " [sec.]");
         
