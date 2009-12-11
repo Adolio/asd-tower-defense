4fad23dfb5dc1ab49abcaf6237a1897351267562
aurelien.dacampo <aurelien.dacampo@02f27a40-d44b-11de-ba7e-09ca73f1295e>
2009-12-11 12:55:34 +0000

    Integration de la jolie nouvelle map! Merci Larzhar :)

---
+++ trunk/src/models/terrains/Labyrinthe.java
@@ -0,0 +1,40 @@
+package models.terrains;
+
+import java.awt.Rectangle;
+
+/**
+ * Classe de gestion d'un terrain dans le desert.
+ * 
+ * Cette classe herite de la classe Terrain de base.
+ * 
+ * @author Pierre-Dominique Putallaz
+ * @author Aurélien Da Campo
+ * @author Lazhar Farjallah
+ * @version 1.0 | 27 novemenbre 2009
+ * @since jdk1.6.0_16
+ * @see Terrain
+ */
+public class Labyrinthe extends Terrain
+{
+	/**
+	 * Constructeur du terrain dans le desert
+	 */
+	public Labyrinthe()
+	{
+		super(500, 500, "img/cartes/spiral3d.png", 
+			  new Rectangle(460,40,20,80),
+			  new Rectangle(320,280,20,60));
+		
+		
+		ajouterMur(new Rectangle(20,0,460,20));
+		ajouterMur(new Rectangle(0,0,20,500)); 		
+		ajouterMur(new Rectangle(20,480,440,20)); 	
+		ajouterMur(new Rectangle(460,140,20,360)); 
+		
+		ajouterMur(new Rectangle(140,140,340,20));
+		ajouterMur(new Rectangle(120,140,20,240));
+		ajouterMur(new Rectangle(140,360,200,20));
+		ajouterMur(new Rectangle(340,240,20,140));
+		ajouterMur(new Rectangle(240,240,100,20));
+	}
+}
