c085472044c326650e763beb1cf326b49dd68660
lazhar.farjallah <lazhar.farjallah@02f27a40-d44b-11de-ba7e-09ca73f1295e>
2010-04-26 07:34:34 +0000

    ~ Amélioration du serveur d'enregistrement, détails techniques.

--- trunk/src/ServeurEnregistrement/CodeEnregistrement.java
+++ trunk/src/ServeurEnregistrement/CodeEnregistrement.java
@@ -2,13 +2,17 @@ package ServeurEnregistrement;
 
 public interface CodeEnregistrement
 {
+   // Codes d'envoi vers le serveur
+   public final int STOP           = 100;
+   public final int TEST           = 101;
+   public final int ENREGISTRER    = 102;
+   public final int DESENREGISTRER = 103;
+   public final int NOMBRE_PARTIES = 104;
+   public final int INFOS_PARTIES  = 105;
+   public final int AJOUTER_JOUEUR = 106;
    
-   public final int STOP           = 0;
-   public final int TEST           = 1;
-   public final int ENREGISTRER    = 2;
-   public final int DESENREGISTRER = 3;
-   public final int NOMBRE_PARTIES = 4;
-   public final int INFOS_PARTIES  = 5;
-   public final int AJOUTER_JOUEUR = 6;
+   // Codes de réponse du serveur
+   public final int OK     = 200;
+   public final int ERREUR = 201;
    
 }
