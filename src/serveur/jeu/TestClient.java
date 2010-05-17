0a2ebe7eda32a6293c50d41c8a6ad2bca2d37f1b
Pierre-Dominique Putallaz <pierredominique.putallaz@gmail.com>
2010-05-17 09:13:56 +0000

    Dialogue très simple de type PING-PONG entre le serveur de jeu et un client UNIQUE.

---
+++ src/serveur/jeu/TestClient.java
@@ -0,0 +1,36 @@
+package serveur.jeu;
+
+import java.net.ConnectException;
+
+import reseau.Canal;
+import reseau.CanalException;
+
+public class TestClient
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		try
+		{
+			Canal canal = new Canal("localhost",2357,true);
+			System.out.println("Envoi de PING");
+			canal.envoyerString("PING");
+			System.out.println("Récéption : "+canal.recevoirString());
+			System.out.println("Fermeture");
+			canal.fermer();
+		} catch (ConnectException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CanalException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
