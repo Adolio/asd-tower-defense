4b1baf86e81eb080b78f8a4215706a12265e2d7a
Pierre-Dominique Putallaz <pierredominique.putallaz@gmail.com>
2010-05-17 11:56:21 +0000

    Ajout des constantes pour le protocole du serveur de jeu.

---
+++ src/serveur/jeu/ConstantesServeurJeu.java
@@ -0,0 +1,20 @@
+package serveur.jeu;
+
+public interface ConstantesServeurJeu
+{
+	public final int START = 100;
+	public final int STOP = 101;
+	public final int OK = 200;
+	public final int MSG = 301;
+	public final int PLAYER = 302;
+	public final int CREATURE = 303;
+	public final int ANIMATION = 304;
+	public final int WAVE = 305;
+	public final int PLAY = 306;
+	public final int TOWER = 307;
+	public final int TOWER_DEL = 306;
+	public final int TOWER_UP = 307;
+	public final int OBJECT = 350;
+	public final int NO_MONEY = 400;
+	public final int BAD_POS = 401;
+}
