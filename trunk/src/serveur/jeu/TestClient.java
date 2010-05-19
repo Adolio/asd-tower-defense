1862c4b2e03d025423a6c01500a34aed33aefd35
pierredominique.putallaz <pierredominique.putallaz@02f27a40-d44b-11de-ba7e-09ca73f1295e>
2010-05-19 08:23:59 +0000

    ~ Matériel pour l'itération de vendredi.

--- trunk/src/serveur/jeu/TestClient.java
+++ trunk/src/serveur/jeu/TestClient.java
@@ -10,36 +10,42 @@ import reseau.CanalException;
 
 public class TestClient implements ConstantesServeurJeu
 {
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args)
+	public TestClient(int ID)
 	{
+
 		try
 		{
-			Canal canal = new Canal("localhost",2357,true);
-			
+			Canal canal = new Canal("localhost", 2357, true);
+
 			// Envoi de l'ID du clietn
-			canal.envoyerInt(12);
+			canal.envoyerInt(ID);
 			// Récéption de la version du serveur
-			System.out.println("Récéption : "+canal.recevoirString());
-			
-			JSONObject json = new JSONObject();
-			json.put("TYPE", MSG);
-			JSONObject content = new JSONObject();
-			content.put("CIBLE", TO_ALL);
-			content.put("MESSAGE","foo bar");
-			json.put("CONTENU", content);
-			
-			canal.envoyerString(json.toString());
-			
-			String rcp = canal.recevoirString();
-			System.out.println("Récéption : "+rcp);
-			json = new JSONObject(rcp);
-			System.out.println(json.getString("MESSAGE"));
-			System.out.println("Fermeture");
-			
-			canal.fermer();
+			System.out.println("Récéption : " + canal.recevoirString());
+
+			while (true)
+			{
+				JSONObject json = new JSONObject();
+				json.put("TYPE", MSG);
+				JSONObject content = new JSONObject();
+				content.put("CIBLE", TO_ALL);
+				content.put("MESSAGE", "foo bar");
+				json.put("CONTENU", content);
+
+				canal.envoyerString(json.toString());
+
+				String rcp = canal.recevoirString();
+				System.out.println("Récéption : " + rcp);
+				json = new JSONObject(rcp);
+				System.out.print("["+json.getString("PSEUDO")+"] ");
+				System.out.println(json.getString("MESSAGE"));
+				synchronized (this)
+				{
+					wait(1000);					
+				}
+			}
+			// System.out.println("Fermeture");
+
+			// canal.fermer();
 		} catch (ConnectException e)
 		{
 			e.printStackTrace();
@@ -49,8 +55,24 @@ public class TestClient implements ConstantesServeurJeu
 		} catch (JSONException e)
 		{
 			e.printStackTrace();
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		int ID = 0;
+		if (args.length > 0)
+		{
+			ID = Integer.parseInt(args[0]);
 		}
 
+		new TestClient(ID);
 	}
 
 }
