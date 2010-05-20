4de99ae0202809941a432923af004b901c622d4e
poulainro <tuxicoman@gmail.com>
2010-05-20 16:40:35 +0000

    + ajout de fonctions d'envoi de requete

--- src/client/jeu/ClientJeu.java
+++ src/client/jeu/ClientJeu.java
@@ -1,13 +1,14 @@
 package client.jeu;
 
 import java.net.ConnectException;
+import serveur.jeu.ConstantesServeurJeu;
 
 import reseau.Canal;
 import reseau.CanalException;
 
 import org.json.*;
 
-public class ClientJeu {
+public class ClientJeu implements ConstantesServeurJeu {
 	final int ID;
 	Canal canal;
 	private final static String IP_SERVEUR = "localhost";
@@ -39,7 +40,7 @@ public class ClientJeu {
 			
 			
 			System.out.println("Envoi de PING");
-			client.envoyerMessage("PING!", i);
+			client.envoyerMessage("PING!", TO_ALL);
 //			
 //			System.out.println("Récéption : "+canal.recevoirString());
 //			System.out.println("Fermeture");
@@ -50,20 +51,114 @@ public class ClientJeu {
 	
 	//TODO controler les betises de l'expediteur (guillemets, etc..)
 	public void envoyerMessage(String message, int cible){
-		//TODO  remplacer par la constante du package
-		String type = "MSG";
+		try {
+			JSONObject json = new JSONObject();
+			json.put("TYPE", MSG);
+			JSONObject content = new JSONObject();
+			content.put("CIBLE", TO_ALL);
+			content.put("MESSAGE", "foo bar");
+			json.put("CONTENU", content);
+			
+			canal.envoyerString(json.toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		//TODO utiiser org.json
-		canal.envoyerString(
-				"{" +
-					"\"TYPE\" : " + type +
-					"\"ID_Player\" : " + ID +
-					"\"message\" : " +
-					"{" +
-						"\"cible\" : " + cible +
-						"\"message\" : " + message +
-					"}"+
-				"}");
+	}
+	
+	
+	//TODO ID reelement necessaire?
+	public void envoyerEtatJoueur(int etat){
+		try{
+			JSONObject json = new JSONObject();
+			json.put("TYPE", PLAYER);
+			json.put("ETAT", etat);
+			
+			canal.envoyerString(json.toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//TODO revoir le nom des parametres
+	public int envoyerVague(int typeVague){
+		try{
+			JSONObject json = new JSONObject();
+			json.put("TYPE", WAVE);
+			json.put("TYPE_WAVE", typeVague);
+			
+			canal.envoyerString(json.toString());
+			
+			String reponse = canal.recevoirString();
+			json = new JSONObject(reponse);
+			return json.getInt("STATUS");
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		//TODO quel val retourner si erreur
+		return -1;
+	}
+	
+	public void envoyerEtatPartie(int etat){
+		try {
+			JSONObject json = new JSONObject();
+			//TODO GAME au lieu de PLAY?
+			json.put("TYPE", PLAY);
+			json.put("ETAT", etat);
+			
+			canal.envoyerString(json.toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void demanderCreationTour(int x, int y, int type){
+		try {
+			JSONObject json = new JSONObject();
+			json.put("TYPE", TOWER);
+			json.put("X", x);
+			json.put("Y", y);
+			//TODO regarder pour le doublon
+			json.put("SORT", type);
+			
+			canal.envoyerString(json.toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void demanderAmeliorationTour(int idTour){
+		try {
+			JSONObject json = new JSONObject();
+			json.put("TYPE", TOWER_UP);
+			json.put("ID_TOWER", idTour);
+			
+			canal.envoyerString(json.toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//TODO suppressionTour ou venteTour?
+	public void venteTour(int idTour){
+		try {
+			JSONObject json = new JSONObject();
+			//TODO TOWER_SELL au lieu de TOWER_DEL?
+			json.put("TYPE", TOWER_DEL);
+			json.put("ID_TOWER", idTour);
+			
+			canal.envoyerString(json.toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
