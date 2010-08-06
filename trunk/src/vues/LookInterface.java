e3e2e0dae8b3d6bb657cf27240712390decdaf89
aurelien.dacampo@gmail.com <aurelien.dacampo@gmail.com@02f27a40-d44b-11de-ba7e-09ca73f1295e>
2010-08-06 11:08:09 +0000

    ~ Correction d'un bug de gestion des zones rectangulaires dans l'éditeur de terrain
    ~ Regénération de la javadoc et rennommage du répertoire "doc" en "javadoc"
    
    + Ajout d'un bouton reset pour les styles dans les options

--- trunk/src/vues/LookInterface.java
+++ trunk/src/vues/LookInterface.java
@@ -12,25 +12,14 @@ import outils.Configuration;
  */
 public class LookInterface
 {
-    // bleu Color(0,112,112)
-    // vert Color(62,140,28)
-    // gris Color(220,220,220)
-    
-    public static Color COULEUR_DE_FOND_PRI  = new Color(62,140,28);
-    public static Color COULEUR_DE_FOND_SEC = new Color(90,180,50);
-    public static Color COULEUR_DE_FOND_BTN = new Color(20, 20, 20);
+    public static Color COULEUR_DE_FOND_PRI     = new Color(62,140,28);
+    public static Color COULEUR_DE_FOND_SEC     = new Color(90,180,50);
+    public static Color COULEUR_DE_FOND_BTN     = new Color(20, 20, 20);
     
     static
     {
-        /*
-        Configuration.setProperty(Configuration.COULEUR_DE_FOND_P, COULEUR_DE_FOND_PRI.getRGB()+"");
-        Configuration.setProperty(Configuration.COULEUR_DE_FOND_S, COULEUR_DE_FOND_SEC.getRGB()+"");
-        Configuration.setProperty(Configuration.COULEUR_DE_FOND_B, COULEUR_DE_FOND_BTN.getRGB()+"");
-        */
-        
         COULEUR_DE_FOND_PRI   = new Color(Integer.parseInt(Configuration.getProprety(Configuration.COULEUR_DE_FOND_P)));
         COULEUR_DE_FOND_SEC   = new Color(Integer.parseInt(Configuration.getProprety(Configuration.COULEUR_DE_FOND_S)));
         COULEUR_DE_FOND_BTN   = new Color(Integer.parseInt(Configuration.getProprety(Configuration.COULEUR_DE_FOND_B)));
     }
-    
 }
